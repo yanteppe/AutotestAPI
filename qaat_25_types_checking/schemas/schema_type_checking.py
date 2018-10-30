b9ff9711ee4f602a340d94863710bdc54173f941
yanteppe <yanteppe@gmail.com>
2018-10-30 17:52:35 +0500

    edit schema

--- qaat_25_types_checking/schemas/schema_type_checking.py
+++ qaat_25_types_checking/schemas/schema_type_checking.py
@@ -1,26 +1,85 @@
 correct_schema = {
+    "additionalProperties": False,
     "properties": {
         "data": {
-            "required": ["showcases"],
+            "additionalProperties": False,
             "properties": {
-                "showcases": {"type": "array"}
-            },
-            "showcases": {
-                "required": ["items"],
-                "properties": {
-                    "items": {
-                        "type": "array"
-                    }
+                "links": {
+                    "additionalItems": False,
+                    "type": "array"
+                },
+                "showcases": {
+                    "additionalItems": False,
+                    # "items": showcase,
+                    "type": "array"
                 },
                 "items": {
-                    "required": ["total", "type", "title"],
-                    "properties": {
-                        "total": {"type": "integer"},
-                        "type": {"type": "string"},
-                        "title": {"type": "string"},
-                    }
+                    "additionalItems": False,
+                    # "items": schedule,
+                    # "items": {
+                    #     "anyOf": [
+                            # channel,
+                            # channel_package,
+                            # episode,
+                            # genre,
+                            # gift,
+                            # movie,
+                            # offer,
+                            # package,
+                            # person,
+                            # program,
+                            # schedule,
+                            # season,
+                            # serial,
+                            # subscription,
+                            # provider,
+                            # promotion
+                        # ]
+                    # },
+                    "type": "array"
+                },
+                "title": {
+                    "type": "string"
+                },
+                "total": {
+                    "type": "integer"
+                },
+                "type": {
+                    "type": "string"
+                },
+                "description": {
+                    "type": "string"
+                },
+                "urn": {
+                    "type": "string"
+                },
+                "references": {
+                    "additionalItems": False,
+                    "items": {
+                        "additionalProperties": False,
+                        "properties": {
+                            "title": {"type": "string"},
+                            "urn": {"type": "string"},
+                            "type": {"type": "string"}
+                        },
+                        "required": ["title", "urn", "type"],
+                        "type": "object"
+                    },
+                    "type": "array"
                 }
-            }
+            },
+            "required": [
+                "links",
+                "title",
+                "total",
+                "type",
+                "urn"
+            ],
+            "type": "object"
         }
-    }
+    },
+    "required": [
+        "data"
+    ],
+    "type": "object"
 }
